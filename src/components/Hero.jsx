1a258e4fc8e23e4a24f78950087e45b21c03d734
Michael Dave Sumang <michael.sumang@hoolisoftware.com>
2023-06-10 22:22:38 +0800

    Revised hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,31 @@
+import { useState } from 'react';
 import styles from './styles/Hero.module.css';
+import { useEffect } from 'react';
 
 const Hero = () => {
+    const [isBlinking, setIsBlinking] = useState(false);
 
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setIsBlinking(!isBlinking);
+        },1000);
+
+        return () => clearInterval(interval);
+    },[isBlinking])
     return(
         <>
             <section className={styles.heroSection}>
                 <div className={styles.flexContainer}>
                     <div className={styles.heroTextOne}>
-                        <span>Hello, World! I&apos;m</span>
+                        <span>Hello, World!</span>
                     </div>
                     <div className={styles.heroTextTwo}>
                         <span>
-                            <span className={styles.lastNameText}>Sumang </span> {'<'}md{'/>'}
+                            <span className={styles.lastNameText}>I&apos;m MD Sumang</span>
                         </span>
                     </div>
                     <div className={styles.heroTextThree}>
-                        <span>a web developer</span>
+                        <span>{'<'}web developer{'/>'} <span className={isBlinking?styles.isBlinkingOn: styles.isBlinkingOff}>|</span></span>
                     </div>
                 </div>
             </section>
