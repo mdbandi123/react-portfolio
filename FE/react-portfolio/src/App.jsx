e0ba0895206529e29eca83055baed1dff747d162
Michael Dave Sumang <michael.sumang@hoolisoftware.com>
2023-07-03 17:17:04 +0800

    hidden overflow on x axis

--- FE/react-portfolio/src/App.jsx
+++ FE/react-portfolio/src/App.jsx
@@ -4,31 +4,9 @@ import AboutMe from "./components/AboutMe";
 import './App.css';
 import Projects from "./components/Projects";
 import ContactMe from "./components/ContactMe";
-import { useRef, useState, useEffect} from 'react';
-
 
 const App = () => {
-
-  const [isVisible, setIsVisible] = useState(false);
-  const containerRef = useRef(null);
-
-  useEffect(()=>{
-    const observer = new IntersectionObserver(([entry])=>{
-
-      console.log(entry);
-      setIsVisible(entry.isIntersecting)
-    })
-
-    if(containerRef.current) {
-      observer.observe(containerRef.current);
-    } 
-
-    return () => {
-      if(containerRef.current) observer.unobserve(containerRef.current);
-    }
-  },[containerRef])
-
-  return(
+return(
     <>
     <Navbar></Navbar>
     <Hero></Hero>
@@ -36,17 +14,6 @@ const App = () => {
     <Projects></Projects>
     <ContactMe></ContactMe>
     </>
-
-    //
-    // <>
-    // <h1 style={{position:'fixed'}}>{isVisible ? 'Yes' : 'No'}</h1>
-    // <div style={{height:'100vh'}} ref={containerRef}>
-    //     <p style={{textAlign: 'center'}} >Hello</p>
-    // </div>
-    // <div style={{height:'100vh'}}>
-    //     <p style={{textAlign: 'center'}}>goodbye</p>
-    // </div>
-    // </>
   )
 }
 
